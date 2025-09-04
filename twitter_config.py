d3c412324229b87386998697fd29c7417ef6f399
Kowshikraj5333 <kowshikraj80@gmail.com>
2025-09-04 09:38:56 +0530

    Delete twitter_config.py

--- twitter_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Set this environment variable before running (recommended)
-# Windows PowerShell:
-# $env:TWITTER_BEARER_TOKEN="your_bearer_token_here"
-
-BEARER_TOKEN = os.environ.get("TWITTER_BEARER_TOKEN", "")  # preferred
-# fallback: you can paste token here (not recommended for production)
-# BEARER_TOKEN = "paste-your-bearer-token-here"
